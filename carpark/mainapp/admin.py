eabcb43d230efc88f1b9aaaa5d2b354d75c963ba
johnk2280 <johnk2280@gmail.com>
2022-09-04 13:33:11 +0500

    [add] solution for the second task

--- carpark/mainapp/admin.py
+++ carpark/mainapp/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 
 from .models import Vehicle
+from .models import Brand
 
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'release_date', 'mileage', 'cost')
+    def display_brand(self, obj: Vehicle) -> str:
+        return obj.brand
+
+    display_brand.short_description = 'Брэнд'
+    list_display = ('id', 'display_brand', 'release_date', 'mileage', 'cost')
     search_fields = ('release_date',)
 
 
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'car_type',
+        'tank_volume',
+        'load_capacity',
+        'seat_number',
+    )
+    search_fields = (
+        'name',
+        'car_type',
+        'seat_number',
+    )
--- carpark/mainapp/models.py
+++ carpark/mainapp/models.py
@@ -20,6 +20,11 @@ class Vehicle(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
+    brand = models.ForeignKey(
+        'Brand',
+        on_delete=models.CASCADE,
+        verbose_name='Название бренда',
+    )
 
     class Meta:
         db_table = 'vehicles'
@@ -32,4 +37,46 @@ class Vehicle(models.Model):
                f' за {self.cost} руб.'
 
 
+class Brand(models.Model):
+    TYPE_CHOICES = (
+        ('легковой', 'PASSENGER'),
+        ('грузовой', 'CARGO'),
+        ('тягач', 'TRUCK'),
+        ('автобус', 'BUS'),
+        ('спортивный', 'SPORT'),
+    )
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        verbose_name='Название бренда',
+    )
+    car_type = models.CharField(
+        max_length=16,
+        choices=TYPE_CHOICES,
+        verbose_name='Тип автомобиля',
+    )
+    tank_volume = models.PositiveSmallIntegerField(
+        null=False,
+        verbose_name='Объем бака, л',
+    )
+    load_capacity = models.PositiveSmallIntegerField(
+        null=False,
+        verbose_name='Грузоподъемность, кг',
+    )
+    seat_number = models.PositiveSmallIntegerField(
+        null=False,
+        verbose_name='Количество мест',
+    )
+
+    class Meta:
+        db_table = 'brands'
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+    def __str__(self):
+        return f'{self.name} {self.car_type} ' \
+               f'объем бака: {self.tank_volume}, ' \
+               f'грузоподъемность: {self.load_capacity}, ' \
+               f'количество мест: {self.seat_number}'
 
