42be7fc7272b73f311a9202fd6dbffd8b24def94
johnk2280 <johnk2280@gmail.com>
2022-09-05 14:52:34 +0500

    [add] solution for the third task

--- carpark/mainapp/admin.py
+++ carpark/mainapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Vehicle
 from .models import Brand
+from .models import Driver
+from .models import Enterprise
+from .models import Vehicle
 
 
 @admin.register(Vehicle)
@@ -9,9 +11,27 @@ class VehicleAdmin(admin.ModelAdmin):
     def display_brand(self, obj: Vehicle) -> str:
         return obj.brand
 
+    def display_company(self, obj: Vehicle) -> str:
+        return obj.company
+
     display_brand.short_description = 'Брэнд'
-    list_display = ('id', 'display_brand', 'release_date', 'mileage', 'cost')
-    search_fields = ('release_date',)
+    display_company.short_description = 'Предприятие'
+    list_display = (
+        'id',
+        'display_brand',
+        'release_date',
+        'mileage',
+        'cost',
+        'display_company',
+    )
+    search_fields = ('release_date', 'company__name')
+
+    def save_model(self, request, obj, form, change):
+        # TODO: при сохранении в модели должна проходить проверка на то,
+        #  назначен ли активный водитель на автомобиль. Если назначен,
+        #  то должно вызываться исключение, которое будет обрабатываться здесь.
+        #  С выводом необходимого сообщения.
+        obj.save(request=request)
 
 
 @admin.register(Brand)
@@ -28,3 +48,32 @@ class BrandAdmin(admin.ModelAdmin):
         'car_type',
         'seat_number',
     )
+
+
+@admin.register(Enterprise)
+class EnterpriseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'city')
+    search_fields = ('name', 'city')
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    def display_company(self, obj: Driver) -> str:
+        return obj.company.name
+
+    def display_vehicle(self, obj: Driver) -> str:
+        return obj.vehicle
+
+    display_company.short_description = 'Предприятие'
+    display_vehicle.short_description = 'Автомобиль'
+
+    list_display = (
+        'id',
+        'name',
+        'category',
+        'experience',
+        'company',
+        'vehicle',
+        'is_active',
+    )
+    search_fields = ('id', 'name', 'company', 'vehicle', 'category')
--- carpark/mainapp/models.py
+++ carpark/mainapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.db import models
 
 
@@ -25,6 +26,13 @@ class Vehicle(models.Model):
         on_delete=models.CASCADE,
         verbose_name='Название бренда',
     )
+    company = models.ForeignKey(
+        'Enterprise',
+        on_delete=models.CASCADE,
+        verbose_name='Предприятие',
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         db_table = 'vehicles'
@@ -33,8 +41,17 @@ class Vehicle(models.Model):
         ordering = ('-created_at',)
 
     def __str__(self):
-        return f'ТС {self.release_date} г.в. с пробегом {self.mileage} км' \
-               f' за {self.cost} руб.'
+        return f'{self.brand} {self.release_date}'
+
+    def save(self, *args, **kwargs):
+        if not self.drivers.filter(is_active=True):
+            super(Vehicle, self).save(*args, **kwargs)
+        else:
+            messages.error(
+                kwargs['request'],
+                message='Невозможно переназначить предприятие, '
+                'т.к. за автомобилем закреплен активный водитель',
+            )
 
 
 class Brand(models.Model):
@@ -75,8 +92,88 @@ class Brand(models.Model):
         verbose_name_plural = 'Бренды'
 
     def __str__(self):
-        return f'{self.name} {self.car_type} ' \
-               f'объем бака: {self.tank_volume}, ' \
-               f'грузоподъемность: {self.load_capacity}, ' \
-               f'количество мест: {self.seat_number}'
+        return f'{self.name} {self.car_type}'
 
+
+class Enterprise(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        verbose_name='Название компании',
+    )
+    city = models.CharField(
+        max_length=50,
+        null=False,
+        verbose_name='Город',
+    )
+
+    class Meta:
+        db_table = 'enterprises'
+        verbose_name = 'Предприятие'
+        verbose_name_plural = 'Предприятия'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Driver(models.Model):
+    CATEGORY_CHOICES = (
+        ('A', 'A'),
+        ('A1', 'A1'),
+        ('B', 'B'),
+        ('B1', 'B1'),
+        ('C', 'C'),
+        ('C1', 'C1'),
+        ('D', 'D'),
+        ('D1', 'D1'),
+        ('BE', 'BE'),
+        ('CE', 'CE'),
+        ('C1E', 'C1E'),
+        ('DE', 'DE'),
+        ('D1E', 'D1E'),
+        ('M', 'M'),
+        ('Tm', 'Tm'),
+        ('Tb', 'Tb'),
+    )
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        verbose_name='Имя',
+    )
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.CASCADE,
+        verbose_name='Автомобиль',
+        null=True,
+        blank=True,
+        related_name='drivers',
+    )
+    experience = models.PositiveSmallIntegerField(
+        null=False,
+        verbose_name='Стаж вождения',
+    )
+    category = models.CharField(
+        max_length=3,
+        null=False,
+        verbose_name='Категория прав',
+        choices=CATEGORY_CHOICES,
+    )
+    company = models.ForeignKey(
+        Enterprise,
+        on_delete=models.CASCADE,
+        verbose_name='Предприятие',
+    )
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Активен',
+    )
+
+    class Meta:
+        db_table = 'drivers'
+        verbose_name = 'Водитель'
+        verbose_name_plural = 'Водители'
+
+    def __str__(self):
+        return f'{self.name}'
