ca603f9e9be0b52bd43f9606bf9a720831b8362b
johnk2280 <johnk2280@gmail.com>
2022-09-07 19:21:35 +0500

    [add] rest_framework

---
+++ carpark/mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include
+from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+
+from .views import VehicleViewSet
+
+app_name = 'mainapp'
+
+router = DefaultRouter()
+router.register('vehicles', VehicleViewSet, basename='vehicles')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
